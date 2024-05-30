be7e9dd753ce57ad3a91f162ae5bbe25efca39cc
pratikmandi <pratikmandi0984@gmail.com>
2024-05-30 22:42:49 +0530

    Alternative method added: 1976A-Verify Password

--- 1976A - Verify Password.cpp
+++ 1976A - Verify Password.cpp
@@ -17,62 +17,86 @@ typedef long long ll;
 typedef unsigned long long ull;
 #define gcd(a,b) __gcd(a,b)
 #define lcm(a,b) (a*(b/gcd(a,b)))
-#include <string>
-#include <cctype>
 
 //--------------------------------------------------------//
 
-bool isStrongPassword(const string& password) {
-    vc letters, digits;
-
-    for (char ch:password) {
-        if (isdigit(ch)) {
-            digits.pb(ch);
-        } else if (islower(ch)) {
-            letters.pb(ch);
+//METHOD 1: Surprisingly it works ;|
+ 
+void isStrongPassword(const string& password) {
+    for(int i=0; i<password.size()-1; i++){
+        if(password[i]>password[i+1]){
+            cout<<"NO"<<endl; 
+            return;
         }
     }
-
-    for (int i = 1; i<digits.size(); ++i) {
-        if (digits[i]<digits[i-1]) {
-            return false;
-        }
-    }
-
-    for (int i = 1; i<letters.size(); ++i) {
-        if (letters[i]<letters[i-1]) {
-            return false;
-        }
-    }
-
-    bool foundLetter = false;
-    for (char ch:password) {
-        if (islower(ch)) {
-            foundLetter = true;
-        } else if (isdigit(ch) && foundLetter) {
-            return false;
-        }
-    }
-
-    return true;
+    cout<<"YES"<<endl;
 }
-
+ 
 int main() {
     int t;
-    cin >> t;
+    cin>>t;
     
-    while (t--) {
+    while (t--){
         int n;
         string s;
-        
         cin>>n>>s;
-
-        if (isStrongPassword(s)) {
-            cout<<"YES"<<endl;
-        } else {
-            cout<<"NO"<<endl;
-        }
+        isStrongPassword(s);
     }
-
-    return 0;
 }
+
+//METHOD 2:
+
+// bool isStrongPassword(const string& password) {
+//     vc letters, digits;
+
+//     for (char ch:password) {
+//         if (isdigit(ch)) {
+//             digits.pb(ch);
+//         } else if (islower(ch)) {
+//             letters.pb(ch);
+//         }
+//     }
+
+//     for (int i = 1; i<digits.size(); ++i) {
+//         if (digits[i]<digits[i-1]) {
+//             return false;
+//         }
+//     }
+
+//     for (int i = 1; i<letters.size(); ++i) {
+//         if (letters[i]<letters[i-1]) {
+//             return false;
+//         }
+//     }
+
+//     bool foundLetter = false;
+//     for (char ch:password) {
+//         if (islower(ch)) {
+//             foundLetter = true;
+//         } else if (isdigit(ch) && foundLetter) {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
+// int main() {
+//     int t;
+//     cin >> t;
+    
+//     while (t--) {
+//         int n;
+//         string s;
+        
+//         cin>>n>>s;
+
+//         if (isStrongPassword(s)) {
+//             cout<<"YES"<<endl;
+//         } else {
+//             cout<<"NO"<<endl;
+//         }
+//     }
+
+//     return 0;
+// }
