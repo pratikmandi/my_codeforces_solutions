6e4616d8e933b0924cd99cee236a9ffca7f3024b
pratikmandi <pratikmandi0984@gmail.com>
2024-05-31 21:41:19 +0530

    1981A

---
+++ 1981A-Turtle and Piggy Are Playing a Game.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <algorithm>
+#include <cmath>
+#include <utility>
+#include <unordered_set>
+#define pb push_back
+#define eb emplace_back
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+typedef vector<long long> vll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef set<int> si;
+typedef set<char> sc;
+typedef long long ll;
+typedef unsigned long long ull;
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b)))
+
+//--------------------------------------------------------//
+
+void solve()
+{
+    int l,r;
+    cin>>l>>r;
+    int x=floor(log2(r));
+    cout<<x<<endl;
+    
+}
+
+int main(){
+    fastio;
+    
+    int t;
+    cin>>t;
+    
+    while (t--)
+    {
+        solve();
+    }
+}
