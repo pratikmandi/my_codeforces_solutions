382ea6e5a839f52e83973d38c1ba3e33099a3ca9
pratikmandi <pratikmandi0984@gmail.com>
2024-06-10 19:27:17 +0530

    1968C-Assembly via Remainders

---
+++ 1968C-Assembly via Remainders.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <algorithm>
+#include <cmath>
+#include <utility>
+#include <unordered_set>
+#include <unordered_map>
+#define pb push_back
+#define eb emplace_back
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+typedef vector<long long> vll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef set<int> si;
+typedef set<char> sc;
+typedef long long ll;
+typedef unsigned long long ull;
+
+// ================================== take ip/op like vector,pairs directly!==================================//
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD> > &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ================================================//
+
+//I should die
+ll solve(){
+    ll n;
+    cin>>n;
+    vll x(n-1);
+    cin>>x;
+    vll a(n);
+    a[n-1]=1e9;
+    for(int i=n-2;i>=0;i--){
+        a[i]=a[i+1]-x[i];
+    }
+    cout<<a<<endl;
+}
+int main(){
+    fastio;
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
