d0688433af41b7d025dd349a7627cdbf0114ea41
pratikmandi <pratikmandi0984@gmail.com>
2024-06-21 21:55:43 +0530

    1669A,B

---
+++ 1669A-Division.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <algorithm>
+#include <cmath>
+#include <utility>
+#include <unordered_set>
+#include <unordered_map>
+#define pb push_back
+#define eb emplace_back
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+typedef vector<long long> vll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef set<int> si;
+typedef set<char> sc;
+typedef long long ll;
+typedef unsigned long long ull;
+
+// ================================== take ip/op like vector,pairs directly!==================================//
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD> > &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ================================================//
+
+void solve(){
+    int n;
+    cin>>n;
+    if(n >= 1900)
+        cout<<"Division 1"<<endl;
+    else if(1600 <= n && n <= 1899)
+        cout<<"Division 2"<<endl;
+    else if(1400 <= n && n <= 1599)
+        cout<<"Division 3"<<endl;
+        else if(n <= 1399)
+        cout<<"Division 4"<<endl;
+}
+
+int main(){
+    fastio;
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
