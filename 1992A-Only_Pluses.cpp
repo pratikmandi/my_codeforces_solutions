8a2f07a1ecf4a981b43e6d4e8c166e3d3f6bd635
pratikmandi <pratikmandi0984@gmail.com>
2024-07-11 22:12:08 +0530

    Codeforces Round 957 (Div. 3)

---
+++ 1992A-Only_Pluses.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <algorithm>
+#include <cmath>
+#include <utility>
+#include <unordered_set>
+#include <unordered_map>
+#define pb push_back
+#define eb emplace_back
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+typedef vector<long long> vll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef set<int> si;
+typedef set<char> sc;
+typedef long long ll;
+typedef unsigned long long ull;
+
+// ================================== take ip/op like vector,pairs directly!==================================//
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD> > &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ================================================//
+
+int solve(){
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    int max_product=a*b*c;
+    for (int i=0; i<=5; i++) {
+        for (int j=0; j<=5-i; j++) {
+            int k=5-i-j;
+            max_product = max(max_product, (a+i)*(b+j)*(c+k));
+        }
+    }
+    cout << max_product << endl;
+}
+
+int main(){
+    fastio;
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+}
---
+++ 1992B-Angry_Monk.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <algorithm>
+#include <cmath>
+#include <utility>
+#include <unordered_set>
+#include <unordered_map>
+#define pb push_back
+#define eb emplace_back
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+typedef vector<long long> vll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef set<int> si;
+typedef set<char> sc;
+typedef long long ll;
+typedef unsigned long long ull;
+
+// ================================== take ip/op like vector,pairs directly!==================================//
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD> > &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ================================================//
+
+int solve(){
+    int n,k; cin>>n>>k;
+    vi v(k);
+    cin>>v;
+    sort(v.begin(), v.end());
+    int op = 0;
+    for(int i = 0; i<k-1; i++){
+        if(v[i] == 1) 
+            op += 1;
+        else 
+            op +=v[i]*2-1;
+    }
+    cout << op << endl;
+
+}
+int main(){
+    fastio;
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+    
+}
