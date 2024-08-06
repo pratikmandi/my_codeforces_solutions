631a2a67b9842675378d3cf2de649737bbc17ecf
pratikmandi <pratikmandi0984@gmail.com>
2024-08-06 22:18:18 +0530

    1999A,D

---
+++ 1999D_Slavic_s_Exam.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <algorithm>
+#include <cmath>
+#include <utility>
+#include <unordered_set>
+#include <unordered_map>
+#define pb push_back
+#define eb emplace_back
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+typedef vector<long long> vll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef set<int> si;
+typedef set<char> sc;
+typedef long long ll;
+typedef unsigned long long ull;
+
+// ================================== take ip/op like vector,pairs directly!==================================//
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD> > &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ================================================//
+
+int solve(){
+    string s, t;
+    cin >> s;
+    cin>> t;
+
+    int j=0;
+    for (int i = 0; i < s.length(); i++) {
+        if (j < t.length() && (s[i] == t[j] || s[i] == '?')) {
+            if (s[i] == '?') {
+                s[i] = t[j];
+            }
+            j++;
+        } else if (s[i] == '?') {
+            s[i] = 'a';
+        }
+    }
+
+    if (j == t.length()) {
+        cout << "YES" << endl;
+        cout << s << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
+
+int main(){
+    fastio;
+    int testcases; cin>>testcases;
+    while(testcases--){
+        solve();
+    }
+}
